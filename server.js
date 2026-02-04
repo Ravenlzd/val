340599e3eb19da45376f0c2246bf1df38332aad8
Ravenauye <alizadarevan1@gmail.com>
2026-02-04 16:21:04 +0200

    Fixed backend + sendChoice

--- server.js
+++ server.js
@@ -17,10 +17,7 @@ app.get("/api/responses", (req, res) => {
     res.json(responses);
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
-
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
 });
