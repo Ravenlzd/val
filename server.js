efb4bafdfe686a6509f085a0d2d3623a2346664c
Ravenauye <alizadarevan1@gmail.com>
2026-02-04 16:18:36 +0200

    Updated valentine site

--- server.js
+++ server.js
@@ -20,4 +20,7 @@ app.get("/api/responses", (req, res) => {
 app.listen(3000, () => console.log("Server running on port 3000"));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
